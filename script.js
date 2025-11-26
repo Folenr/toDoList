8aff206da06f8e1c39e2d3681ca8b5b8a7e58412
Folenr <t.a.r252e@gmail.com>
2025-11-26 19:32:22 +0200

    added the ability to create tasks in lists and move them

--- script.js
+++ script.js
@@ -1,5 +1,55 @@
-function Submit(){
-    const parg = document.createElement('p');
-    parg.innerText = document.getElementById("input").value;
-    document.body.appendChild(parg);
+function createTask(){
+    const toDo = document.getElementById('toDo');
+    const addButton = document.getElementById('addTask');
+    const div = document.createElement('div');
+    const input = document.createElement('input');
+    const button = document.createElement('button');
+
+    button.innerText = 'submit';
+    button.onclick = function(){Submit(input,button);};
+    div.className = 'task';
+
+    toDo.appendChild(div);
+    div.appendChild(input);
+    div.appendChild(button);
+    toDo.appendChild(addButton);
+}
+
+function Submit(input,button){
+    const p = document.createElement('p');
+    const label = document.createElement('label');
+    const select = document.createElement('select');
+    const op1 = document.createElement('option');
+    const op2 = document.createElement('option');
+    const op3 = document.createElement('option');
+
+    op1.value = 'toDo';
+    op1.innerText = 'to do';
+    op2.value = 'doing';
+    op2.innerText = 'doing';
+    op3.value = 'done';
+    op3.innerText = 'done';
+
+    select.onchange = function() {changeList(select);};
+    label.className = 'select';
+    p.innerText = input.value;
+
+    select.appendChild(op1);
+    select.appendChild(op2);
+    select.appendChild(op3);
+    label.appendChild(select);
+    input.parentNode.appendChild(p);
+    input.parentNode.appendChild(label);
+    input.remove();
+    button.remove();
+
+}
+
+function changeList(select){
+    const div = select.parentNode.parentNode;
+    const list = document.getElementById(select.value);
+    const addButton = document.getElementById('addTask');
+    list.appendChild(div);
+    if(list.id == 'toDo')
+        list.appendChild(addButton);
 }
