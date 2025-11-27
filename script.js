e03091689bffebb2a70d473ca2b9eb42b16ffa84
Folenr <t.a.r252e@gmail.com>
2025-11-27 04:40:51 +0200

    added new fonts and updated the UI

--- script.js
+++ script.js
@@ -22,13 +22,16 @@ function Submit(input,button){
     const op1 = document.createElement('option');
     const op2 = document.createElement('option');
     const op3 = document.createElement('option');
-
+    const op4 = document.createElement('option');
+    
     op1.value = 'toDo';
     op1.innerText = 'to do';
     op2.value = 'doing';
     op2.innerText = 'doing';
     op3.value = 'done';
     op3.innerText = 'done';
+    op4.value = 'delete';
+    op4.innerText = 'delete';
 
     select.onchange = function() {changeList(select);};
     label.className = 'select';
@@ -37,6 +40,7 @@ function Submit(input,button){
     select.appendChild(op1);
     select.appendChild(op2);
     select.appendChild(op3);
+    select.appendChild(op4);
     label.appendChild(select);
     input.parentNode.appendChild(p);
     input.parentNode.appendChild(label);
@@ -49,7 +53,10 @@ function changeList(select){
     const div = select.parentNode.parentNode;
     const list = document.getElementById(select.value);
     const addButton = document.getElementById('addTask');
-    list.appendChild(div);
-    if(list.id == 'toDo')
+    if(select.value != 'delete'){
+        list.appendChild(div);
+        if(list.id == 'toDo')
         list.appendChild(addButton);
+    }else
+        div.remove();
 }
