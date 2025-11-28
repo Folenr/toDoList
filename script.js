7c173a9e5b33d45dee443ec2c6321709bb850789
Folenr <t.a.r252e@gmail.com>
2025-11-28 18:49:28 +0200

    added extra buttons and upgraded the UI even more

--- script.js
+++ script.js
@@ -2,10 +2,11 @@ function createTask(){
     const toDo = document.getElementById('toDo');
     const addButton = document.getElementById('addTask');
     const div = document.createElement('div');
-    const input = document.createElement('input');
+    const input = document.createElement('textarea');
     const button = document.createElement('button');
 
-    button.innerText = 'submit';
+    input.onkeyup = function() {textAreaAdjust(input);};
+    button.innerText = 'S';
     button.onclick = function(){Submit(input,button);};
     div.className = 'task';
 
@@ -22,7 +23,8 @@ function Submit(input,button){
     const op1 = document.createElement('option');
     const op2 = document.createElement('option');
     const op3 = document.createElement('option');
-    const op4 = document.createElement('option');
+    const deleteButton = document.createElement('button');
+    const editButton = document.createElement('button');
     
     op1.value = 'toDo';
     op1.innerText = 'to do';
@@ -30,8 +32,14 @@ function Submit(input,button){
     op2.innerText = 'doing';
     op3.value = 'done';
     op3.innerText = 'done';
-    op4.value = 'delete';
-    op4.innerText = 'delete';
+    
+    deleteButton.onclick = function() {deleteTask(deleteButton);};
+    deleteButton.innerText = 'D';
+    deleteButton.className = 'taskButton';
+
+    editButton.onclick = function() {editTask(editButton);};
+    editButton.innerText = 'E';
+    editButton.className = 'taskButton';
 
     select.onchange = function() {changeList(select);};
     label.className = 'select';
@@ -40,10 +48,11 @@ function Submit(input,button){
     select.appendChild(op1);
     select.appendChild(op2);
     select.appendChild(op3);
-    select.appendChild(op4);
     label.appendChild(select);
     input.parentNode.appendChild(p);
     input.parentNode.appendChild(label);
+    input.parentNode.appendChild(deleteButton);
+    input.parentNode.appendChild(editButton);
     input.remove();
     button.remove();
 
@@ -53,10 +62,34 @@ function changeList(select){
     const div = select.parentNode.parentNode;
     const list = document.getElementById(select.value);
     const addButton = document.getElementById('addTask');
-    if(select.value != 'delete'){
-        list.appendChild(div);
-        if(list.id == 'toDo')
+    list.appendChild(div);
+    if(list.id == 'toDo')
         list.appendChild(addButton);
-    }else
-        div.remove();
+}
+
+function textAreaAdjust(element) {
+    element.style.minHeight = '1px';
+    element.style.minHeight = (element.scrollHeight)+"px";
+}
+
+function deleteTask(button){
+    button.parentNode.remove();
+}
+
+function editTask(button){
+    const div = button.parentNode;
+    const input = document.createElement('textarea');
+    const submitButton = document.createElement('button');
+    
+    submitButton.innerText = 'S';
+    submitButton.onclick = function(){Submit(input,submitButton);};
+    input.value = div.querySelector('p').innerText;
+    input.onkeyup = function() {textAreaAdjust(input);};
+    div.appendChild(input);
+    div.appendChild(submitButton);
+    textAreaAdjust(input)
+
+    div.querySelectorAll('.taskButton').forEach(taskButton => taskButton.remove());
+    div.querySelector('p').remove();
+    div.querySelector('label').remove();
 }
